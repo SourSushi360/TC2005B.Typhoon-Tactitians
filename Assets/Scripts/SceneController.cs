1c6442662738b6f341445c4494f784965cfd3c47
Jacob <a01643891@tec.mx>
2024-04-28 16:56:21 -0600

    Implemented initial scene changing

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void NextScene(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PreviousScene(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
+    }
+}
