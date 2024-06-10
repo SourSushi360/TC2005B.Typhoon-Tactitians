f2e1194d7f7078d16e0fa251f44015759b3e87ca
SourSushi360 <jplopez365@hotmail.com>
2024-06-10 16:06:38 -0600

    final level layout

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class Food : MonoBehaviour
 {
     public int value = 1;
-    public GameManager gameManager;
 
     public void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")) {
-            gameManager.SumarPuntos(value);
+            GameManager.Instance.SumarPuntos(value);
             Destroy(this.gameObject);
         }
     }
