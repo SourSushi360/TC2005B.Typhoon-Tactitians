fbb69948a6f2a6a6a38d0571b2e168097121b906
SourSushi360 <jplopez365@hotmail.com>
2024-06-10 22:25:48 -0600

    I TRUST IN GOD

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -20,7 +20,6 @@ public class PlayerMovement : MonoBehaviour
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
         Animator = GetComponent<Animator>();
-        // Used to calculate the offset of the hitbox for the ground collision
         hitbox = GetComponent<Collider2D>();
         hitboxOffset = hitbox.offset.x;
         health += GameData.upgradeLevels["suitUpgrade"];
@@ -31,55 +30,80 @@ public class PlayerMovement : MonoBehaviour
         Horizontal = Input.GetAxisRaw("Horizontal");
         Animator.SetBool("running", Horizontal != 0.0f);
 
-        if (Horizontal < 0.0f) {
+        if (Horizontal < 0.0f)
+        {
             transform.localScale = new Vector3(-0.5f, 0.5f, 1.0f);
-            hitboxOffset = hitbox.offset.x  * -1;
-        } else if (Horizontal > 0.0f) {
+            hitboxOffset = hitbox.offset.x * -1;
+        }
+        else if (Horizontal > 0.0f)
+        {
             transform.localScale = new Vector3(0.5f, 0.5f, 1.0f);
             hitboxOffset = hitbox.offset.x;
         }
 
         Vector3 rayStart = new Vector3(Rigidbody2D.position.x + hitboxOffset, Rigidbody2D.position.y, 0);
-        
-        //Debug.DrawRay(rayStart, Vector3.down, Color.white);
-        if (Physics2D.Raycast(rayStart, Vector3.down, 0.1f)) {
+
+        if (Physics2D.Raycast(rayStart, Vector3.down, 0.1f))
+        {
             Grounded = true;
-        } else Grounded = false;
-        if (Input.GetKeyDown(KeyCode.W) && Grounded) {
+        }
+        else
+        {
+            Grounded = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && Grounded)
+        {
             Jump();
         }
 
-        if (Input.GetKeyDown(KeyCode.S)) {
-            if(touchingStair){
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (touchingStair)
+            {
                 GameManager.Instance.PreviousScene();
             }
         }
-
-    }
-    private void Jump() {
-        Rigidbody2D.velocity = new Vector2(0,0);
-        Rigidbody2D.AddForce(Vector2.up*JumpForce);
     }
 
-    private void FixedUpdate() {
-        Rigidbody2D.velocity = new Vector2(Horizontal*Speed,Rigidbody2D.velocity.y);
+    private void Jump()
+    {
+        Rigidbody2D.velocity = new Vector2(0, 0);
+        Rigidbody2D.AddForce(Vector2.up * JumpForce);
     }
-    public void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Finish")) {
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Finish"))
+        {
             touchingStair = true;
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other) {
-        if (other.CompareTag("Finish")) {
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Finish"))
+        {
             touchingStair = false;
         }
     }
-    
-    public void getHit(int dmg){
+
+    public void getHit(int dmg)
+    {
         health -= dmg;
-        if(health <= 0){
+        if (health <= 0)
+        {
             GameManager.Instance.PlayerDied();
         }
+    }
+
+    public void PushBack(Vector2 direction, float force)
+    {
+        Rigidbody2D.AddForce(direction * force, ForceMode2D.Impulse);
     }
 }
--- Assets/Scripts/Tornado.cs
+++ Assets/Scripts/Tornado.cs
@@ -6,29 +6,40 @@ public class Tornado : MonoBehaviour
 {
     private int elapsedTime;
 
-    [SerializeField] int dmg;
-    // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(Timer());
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
     }
 
     public IEnumerator Timer()
     {
-        while(true){
+        while (true)
+        {
             elapsedTime += 1;
-            
-            if(elapsedTime % 3 == 0){
+
+            if (elapsedTime % 3 == 0)
+            {
                 Destroy(gameObject);
             }
             yield return new WaitForSeconds(1);
         }
-        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerMovement player = collision.gameObject.GetComponent<PlayerMovement>();
+            if (player != null)
+            {
+                Vector2 direction = Vector2.up;
+                player.PushBack(direction, 0.8f);
+                player.getHit(1);
+            }
+        }
     }
 }
